241b117db8e9318b7a57a3175133ddfe70ae2073
Natalia Ivanova <natalya.ivanova@stm-labs.ru>
2020-03-13 11:29:30 +0300

    first init

---
+++ src/components/Form.vue
@@ -0,0 +1,258 @@
+<template>
+  <q-card class="my-card" v-if="action">
+    <div class="q-pa-md">
+      <div class="text-h6">{{action}}</div>
+      <q-form v-if="action === 'CREATE'"
+        @submit="onSubmitCreate"
+        @reset="onReset"
+        class="q-gutter-md"
+      >
+        <q-input
+          id="name"
+          v-model="name"
+          label="Наименование переменной"
+          required
+          bottom-slots
+          hint="Поле должно содержать только латинские буквы и знак '_'"
+          :error="!isValidName"
+          error-message="Поле должно содержать только латинские буквы и знак '_'"
+        >
+        </q-input>
+
+        <q-select
+          v-model="type"
+          :options="types"
+          label="Тип"
+          :rules="[ val => val !== null || 'Please use maximum 3 characters' ]" />
+        <q-input
+          v-model.number="size"
+          type="number"
+          label="Размер"
+          filled
+        />
+        <q-input
+          filled
+          v-model="value"
+          label="Значение по умолчанию"
+          bottom-slots
+          hint="Значение должно соответствовать типу переменной"
+          :error="!isValidValue"
+          error-message="Значение не соответствует типу переменной."
+        />
+        <q-input
+          v-model="description"
+          filled
+          type="textarea"
+          label="Описание"
+        />
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+
+      <q-form v-if="action === 'SET'"
+        @submit="onSubmitSet"
+        @reset="onReset"
+        class="q-gutter-md"
+      >
+        <q-select v-model="variableSet" :options="allVariables" label="Выберите переменную" />
+        <q-input
+          filled
+          v-model="valueSet"
+          label="Новое значение"
+        />
+        <q-input
+          v-model="descriptionSet"
+          filled
+          type="textarea"
+          label="Описание"
+        />
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+
+      <q-form v-if="action === 'REPLACE'"
+              @submit="onSubmitReplace"
+              @reset="onReset"
+              class="q-gutter-md"
+      >
+        <q-select v-model="variableReplace" :options="strVariables" label="Выберите переменную" />
+        <q-input
+          filled
+          v-model="oldStr"
+          label="Заменяемая строка"
+        />
+        <q-input
+          filled
+          v-model="newStr"
+          label="Заменяющая строка"
+        />
+        <q-select v-model="resVariable" :options="strVariables" label="Результирующая переменная" />
+        <q-input
+          v-model="descriptionReplace"
+          filled
+          type="textarea"
+          label="Описание"
+        />
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+
+      <q-form v-if="action === 'BITWISE'"
+              @submit="onSubmitBitwise"
+              @reset="onReset"
+              class="q-gutter-md"
+      >
+        <q-select v-model="variableBit" :options="allVariables" label="Выберите переменную" />
+        <q-select v-model="typeBit" :options="typesBit" label="Тип сдвига" />
+        <q-input
+          filled
+          v-model="mask"
+          label="Маска (16-ричный формат)"
+          bottom-slots
+          hint="Поле должно содержать число в 16-ричный формате"
+          :error="!isValidMask"
+          error-message="Поле должно содержать число в 16-ричный формате"
+        />
+        <q-select
+          v-model="resVariableBit"
+          :options="allVariables"
+          label="Результирующая переменная" />
+        <q-input
+          v-model="descriptionBitwise"
+          filled
+          type="textarea"
+          label="Описание"
+        />
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+    </div>
+  </q-card>
+</template>
+
+<script>
+import { uid } from 'quasar';
+
+export default {
+  name: 'Form',
+  props: {
+    action: String,
+    allVariables: Array,
+    data: Array,
+    strVariables: Array,
+    editing: Boolean,
+    editingArr: Array,
+  },
+  data() {
+    return {
+      name: '',
+      type: null,
+      types: [
+        'boolean', 'bytes', 'double', 'integer', 'long', 'string',
+      ],
+      size: 0,
+      value: '',
+      description: '',
+      valueSet: null,
+      variableSet: null,
+      descriptionSet: '',
+      variableReplace: null,
+      oldStr: '',
+      newStr: '',
+      resVariable: null,
+      descriptionReplace: '',
+      variableBit: null,
+      typeBit: null,
+      typesBit: [
+        'AND', 'OR', 'XOR',
+      ],
+      mask: '',
+      resVariableBit: null,
+      descriptionBitwise: '',
+    };
+  },
+  computed: {
+    isValidName() {
+      return /[A-Za-z_]+$/.test(this.name) || !this.name;
+    },
+    isValidMask() {
+      return /[0-9ABCDF]+$/.test(this.mask) || !this.mask;
+    },
+    isValidValue() {
+      if (this.type === 'boolean') {
+        return /^(true|false)$/.test(this.value) || !this.value;
+      } if (this.type === 'bytes') {
+        return /^[ 0-1]+$/.test(this.value) || !this.value;
+      } if (this.type === 'double') {
+        return /^\d+(?:[.,]\d+)?$/.test(this.value) || !this.value;
+      } if ((this.type === 'integer') || ((this.type === 'long'))) {
+        return /^[-+]?\d*$/.test(this.value) || !this.value;
+      } if (this.type === 'string') {
+        return true;
+      }
+      return true;
+    },
+  },
+  methods: {
+    onSubmitCreate() {
+      this.newCreateObj = {
+        id: uid(),
+        action: 'CREATE',
+        name: this.name,
+        type: this.type,
+        size: this.size,
+        value: this.value,
+        description: this.description,
+        actions: [],
+      };
+      this.$emit('create', this.newCreateObj);
+    },
+    onSubmitSet() {
+      this.newSetObj = {
+        action: 'SET',
+        variableName: this.variableSet,
+        newValue: this.valueSet,
+        description: this.descriptionSet,
+      };
+      this.$emit('add', this.variableSet, this.newSetObj);
+    },
+    onSubmitReplace() {
+      this.newReplaceObj = {
+        action: 'REPLACE',
+        variableName: this.variableReplace,
+        oldStr: this.oldStr,
+        newStr: this.newStr,
+        variableResult: this.resVariable,
+        description: this.descriptionReplace,
+      };
+      this.$emit('add', this.variableReplace, this.newReplaceObj);
+    },
+    onSubmitBitwise() {
+      this.newBitwiseObj = {
+        action: 'BITWISE',
+        variableName: this.variableBit,
+        operation: this.typeBit,
+        mask: this.mask,
+        variableResult: this.resVariableBit,
+        description: this.descriptionBitwise,
+      };
+      this.$emit('add', this.variableBit, this.newBitwiseObj);
+    },
+    // to do
+    onReset() {
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,272 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md">
+      <q-table
+        title="Список действий"
+        :data="data"
+        :columns="columns"
+        row-key="id"
+        :filter="search"
+      >
+        <template v-slot:header="props">
+          <q-tr :props="props">
+            <q-th auto-width />
+            <q-th
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+            >
+              {{ col.label }}
+            </q-th>
+            <q-th>Редактировать</q-th>
+            <q-th>Удалить</q-th>
+          </q-tr>
+        </template>
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td auto-width>
+              <q-btn size="sm"
+                     color="primary"
+                     round
+                     dense
+                     @click="props.expand = !props.expand"
+                     :icon="props.expand ? 'remove' : 'add'" />
+            </q-td>
+            <q-td
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+            >
+              {{ col.value }}
+            </q-td>
+            <q-td class="text-center">
+              <q-btn
+                size="sm"
+                round
+                color="primary"
+                icon="edit"
+                @click="edit()" />
+            </q-td>
+            <q-td class="text-center">
+              <q-btn
+                size="sm"
+                round
+                color="primary"
+                icon="remove"
+                @click="remove(props.row.id, data)" />
+            </q-td>
+          </q-tr>
+          <q-tr
+            v-for="(actionObj, index) in props.row.actions"
+            :key="index"
+            v-show="props.expand"
+            :props="props" >
+            <q-td>
+              <q-btn
+                size="sm"
+                round
+                color="primary"
+                icon="arrow_upward"
+                @click="moveToTheTop(props.row, actionObj, index)" />
+            </q-td>
+            <q-td colspan="6">
+              <ul>
+                <li
+                  v-for="(value, index) in actionObj"
+                  :key="index">{{fields[index]}}: {{value}}
+                </li>
+              </ul>
+            </q-td>
+            <q-td class="text-center">
+              <q-btn
+                size="sm"
+                round
+                color="primary"
+                icon="edit"
+                @click="edit(index, props.row)" />
+            </q-td>
+            <q-td class="text-center">
+              <q-btn
+                size="sm"
+                round
+                color="primary"
+                icon="remove"
+                @click="remove(index, props.row)" />
+            </q-td>
+          </q-tr>
+        </template>
+        <template v-slot:top-right>
+          <q-input borderless dense debounce="300" v-model="search" placeholder="Search">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+        <template v-slot:bottom>
+          NEXT
+        </template>
+      </q-table>
+    </div>
+
+    <div class="q-pa-md">
+    <q-btn-dropdown color="primary" label="Выберите действие:">
+      <q-list>
+        <q-item clickable v-close-popup @click="onChangeAction('CREATE')">
+          <q-item-section>
+            <q-item-label>CREATE</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onChangeAction('SET')">
+          <q-item-section>
+            <q-item-label>SET</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onChangeAction('BITWISE')">
+          <q-item-section>
+            <q-item-label>BITWISE</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onChangeAction('REPLACE')">
+          <q-item-section>
+            <q-item-label>REPLACE</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+      <Form
+        :action=action
+        :allVariables=variables
+        :strVariables=strVariables
+        :data=data
+        :editing=editing
+        :editingArr=editingArr
+        @add="add"
+        @create="create"
+       />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Form from '../components/Form.vue';
+
+export default {
+  name: 'PageIndex',
+  components: {
+    Form,
+  },
+  data() {
+    return {
+      search: '',
+      editing: false,
+      editingArr: [],
+      columns: [
+        {
+          name: 'action', align: 'center', label: 'Действие', field: 'action',
+        },
+        {
+          name: 'name', align: 'center', label: 'Наименование переменной', field: 'name',
+        },
+        {
+          name: 'type', align: 'center', label: 'Тип', field: 'type',
+        },
+        {
+          name: 'size', align: 'center', label: 'Размер', field: 'size',
+        },
+        {
+          name: 'value', align: 'center', label: 'Значение', field: 'value',
+        },
+        {
+          name: 'description', align: 'center', label: 'Описание', field: 'description',
+        },
+      ],
+      data: [],
+      action: '',
+      variables: [],
+      strVariables: [],
+      removeConf: false,
+      confirmVar: false,
+      fields: {
+        action: 'Действие',
+        variableName: 'Имя переменной',
+        newValue: 'Новое значение',
+        description: 'Описание',
+        oldStr: 'Заменяемая строка',
+        newStr: 'Заменяющая строка',
+        variableResult: 'Результирующая переменная',
+        operation: 'Побитовая операция',
+        mask: 'Маска',
+      },
+    };
+  },
+  methods: {
+    onChangeAction(action) {
+      this.action = action;
+    },
+    create(data) {
+      this.data.push(data);
+      this.getAllVariables();
+    },
+    add(variable, data) {
+      const index = this.data.findIndex((item) => item.name === variable);
+      this.data[index].actions.push(data);
+      this.alert('Действие добавлено!');
+    },
+    getAllVariables() {
+      const createdVariables = this.data.filter((item) => item.action === 'CREATE');
+      this.variables = createdVariables.map((item) => item.name);
+      this.strVariables = createdVariables.filter((item) => item.type === 'string').map((item) => item.name);
+    },
+    moveToTheTop(obj, action, ind) {
+      if (ind === 0) {
+        obj.actions.shift();
+        obj.actions.push(action);
+      } else {
+        obj.actions.splice(ind - 1, 2, obj.actions[ind], obj.actions[ind - 1]);
+      }
+    },
+    edit(ind, data) {
+      this.editing = true;
+      this.editingArr = [ind, data];
+      this.action = data.actions[ind].action;
+    },
+    remove(ind, data) {
+      let message = '';
+      let updateVars;
+      if (data.actions) {
+        message = 'Вы точно хотите удалить это действие?';
+        updateVars = false;
+        this.confirmDelete(ind, message, data.actions, updateVars);
+      } else {
+        const deleteIndex = this.data.findIndex((item) => item.id === ind);
+        message = 'Если Вы удалите CREATE, то все действия с этой переменной автоматически удалятся. Вы точно хотите это сделать?';
+        updateVars = true;
+        this.confirmDelete(deleteIndex, message, data, updateVars);
+      }
+    },
+    confirmDelete(ind, message, data, update) {
+      this.$q.dialog({
+        title: 'Внимание',
+        message,
+        cancel: true,
+        persistent: true,
+      }).onOk(() => {
+        data.splice(ind, 1);
+        if (update) {
+          this.getAllVariables();
+        }
+      });
+    },
+    alert(message) {
+      this.$q.dialog({
+        message,
+      });
+    },
+  },
+
+
+};
+</script>
